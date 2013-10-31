102e8e3655cc281e8e15ec57ccb28e2e34f10d5e
Thomas Friedel <thomas.friedel@berlin>
2013-10-31 15:41:03 +0100

    .

---
+++ include/traceUtil.h
@@ -0,0 +1,23 @@
+#ifndef TRACE_UTIL_H
+#define TRACE_UTIL_H
+// extern __thread 
+
+#include <iostream>
+#include <string>
+static unsigned short trace_current_nesting;
+
+static inline void trace_increment_nesting_level(void)
+{
+    trace_current_nesting++;
+}
+
+static inline void trace_decrement_nesting_level(void)
+{
+    trace_current_nesting--;
+}
+
+static inline unsigned short trace_get_nesting_level(void)
+{
+    return trace_current_nesting;
+}
+#endif
